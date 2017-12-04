c61370b38fccbadba091f021b839e4b56bdaf5a7
qmei1123 <qmei@bu.edu>
2017-12-04 17:18:47 -0500

    js

--- javascript/weather.js
+++ javascript/weather.js
@@ -67,15 +67,6 @@ async function getWeather (city, key, units = 'imperial') {
     } else {
       document.body.className = 'hot'
     }
-
-      var coldRun = document.createTextNode('Put on a jacket, suck it up, and run like the wind.')
-      var tempRange = document.getElementsByClassName('temp-range')
-      
-      function coldRunDiv = document.tempRange.appendChild(coldRun);
-      
-      if (document.body.className = 'cold') {
-          coldRunDiv;
-      }
       
     // Set Weather Data
     conditionDescription.innerText = data.weather[0].description
