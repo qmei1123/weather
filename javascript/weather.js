097a9871898310acb4e004a3fc4824e240cd96d1
qmei1123 <qmei@bu.edu>
2017-12-04 17:18:02 -0500

    js

--- javascript/weather.js
+++ javascript/weather.js
@@ -77,9 +77,6 @@ async function getWeather (city, key, units = 'imperial') {
           coldRunDiv;
       }
       
-      if (document.body.className = 'cold') {
-          document.body.createTextNode('Put on a jacket, suck it up, and run like the wind.')
-      }
     // Set Weather Data
     conditionDescription.innerText = data.weather[0].description
     windSpeed.innerText = Math.round(data.wind.speed)
